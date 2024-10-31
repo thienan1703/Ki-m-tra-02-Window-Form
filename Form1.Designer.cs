952d51a01ecdb0520db484d22406d9172435ef90
thienan1703 <dangducthienan17032004@gmail.com>
2024-11-01 00:21:41 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Window_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lstProducts = new ListBox();
+            lstCart = new ListBox();
+            btnAddToCart = new Button();
+            btnRemoveFromCart = new Button();
+            btnCheckout = new Button();
+            lblTotalPrice = new Label();
+            SuspendLayout();
+            // 
+            // lstProducts
+            // 
+            lstProducts.FormattingEnabled = true;
+            lstProducts.Location = new Point(26, 27);
+            lstProducts.Name = "lstProducts";
+            lstProducts.Size = new Size(508, 144);
+            lstProducts.TabIndex = 0;
+            // 
+            // lstCart
+            // 
+            lstCart.FormattingEnabled = true;
+            lstCart.Location = new Point(569, 27);
+            lstCart.Name = "lstCart";
+            lstCart.Size = new Size(288, 144);
+            lstCart.TabIndex = 1;
+            // 
+            // btnAddToCart
+            // 
+            btnAddToCart.Location = new Point(322, 241);
+            btnAddToCart.Name = "btnAddToCart";
+            btnAddToCart.Size = new Size(153, 29);
+            btnAddToCart.TabIndex = 2;
+            btnAddToCart.Text = "Thêm vào giỏ hàng";
+            btnAddToCart.UseVisualStyleBackColor = true;
+            btnAddToCart.Click += btnAddToCart_Click;
+            // 
+            // btnRemoveFromCart
+            // 
+            btnRemoveFromCart.Location = new Point(556, 241);
+            btnRemoveFromCart.Name = "btnRemoveFromCart";
+            btnRemoveFromCart.Size = new Size(155, 29);
+            btnRemoveFromCart.TabIndex = 3;
+            btnRemoveFromCart.Text = "Xoá khỏi giỏ hàng";
+            btnRemoveFromCart.UseVisualStyleBackColor = true;
+            btnRemoveFromCart.Click += btnRemoveFromCart_Click_1;
+            // 
+            // btnCheckout
+            // 
+            btnCheckout.Location = new Point(451, 357);
+            btnCheckout.Name = "btnCheckout";
+            btnCheckout.Size = new Size(94, 29);
+            btnCheckout.TabIndex = 4;
+            btnCheckout.Text = "Thanh toán";
+            btnCheckout.UseVisualStyleBackColor = true;
+            // 
+            // lblTotalPrice
+            // 
+            lblTotalPrice.AutoSize = true;
+            lblTotalPrice.Location = new Point(322, 318);
+            lblTotalPrice.Name = "lblTotalPrice";
+            lblTotalPrice.Size = new Size(160, 20);
+            lblTotalPrice.TabIndex = 5;
+            lblTotalPrice.Text = " Tổng giá trị đơn hàng:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(918, 450);
+            Controls.Add(lblTotalPrice);
+            Controls.Add(btnCheckout);
+            Controls.Add(btnRemoveFromCart);
+            Controls.Add(btnAddToCart);
+            Controls.Add(lstCart);
+            Controls.Add(lstProducts);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+            this.btnRemoveFromCart.Click += new System.EventHandler(this.btnRemoveFromCart_Click);
+            this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click);
+
+        }
+
+        #endregion
+
+        private ListBox lstProducts;
+        private ListBox lstCart;
+        private Button btnAddToCart;
+        private Button btnRemoveFromCart;
+        private Button btnCheckout;
+        private Label lblTotalPrice;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿namespace Window_Form
+{
+    public partial class Form1 : Form
+    {
+        private ShoppingCart cart = new ShoppingCart();
+        private List<Product> products = new List<Product>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProducts();
+            DisplayProducts();
+        }
+
+        private void LoadProducts()
+        {
+            products.Add(new Product("sp1", 100000, 1));
+            products.Add(new Product("sp2", 200000, 1));
+            products.Add(new Product("sp3", 300000, 1));
+            products.Add(new Product("sp4", 400000, 1));
+        }
+
+        private void DisplayProducts()
+        {
+            lstProducts.Items.Clear();
+            foreach (var product in products)
+            {
+                lstProducts.Items.Add(product);
+            }
+        }
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            if (lstProducts.SelectedItem is Product selectedProduct)
+            {
+                cart.AddToCart(new Product(selectedProduct.Name, selectedProduct.Price, 1));
+                DisplayCart();
+                UpdateTotalPrice();
+            }
+        }
+
+        private void btnRemoveFromCart_Click(object sender, EventArgs e)
+        {
+            if (lstCart.SelectedItem != null)
+            {
+
+                var selectedCartItem = lstCart.SelectedItem.ToString();
+                string productName = selectedCartItem.Split('-')[0].Trim();
+                cart.RemoveFromCart(productName);
+                DisplayCart();
+                UpdateTotalPrice();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm trong giỏ hàng để xóa.", "Thông báo");
+            }
+        }
+
+        private void DisplayCart()
+        {
+            lstCart.Items.Clear();
+            foreach (var item in cart.GetCartItems())
+            {
+                lstCart.Items.Add($"{item.Name} - Số lượng: {item.Quantity} - Giá: {item.Price * item.Quantity} VND");
+            }
+        }
+
+        private void UpdateTotalPrice()
+        {
+            lblTotalPrice.Text = "Tổng giá trị: " + cart.CalculateTotal() + " VND";
+        }
+
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có chắc chắn muốn thanh toán không?", "Xác nhận thanh toán", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("Đơn hàng của bạn đã được hoàn tất!", "Thông báo");
+                cart.ClearCart();
+                DisplayCart();
+                UpdateTotalPrice();
+            }
+        }
+        public class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+
+            public Product(string name, decimal price, int quantity)
+            {
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} - Giá: {Price} VND";
+            }
+        }
+
+        public class ShoppingCart
+        {
+            private List<Product> cartItems = new List<Product>();
+
+            public void AddToCart(Product product)
+            {
+                var existingProduct = cartItems.Find(p => p.Name == product.Name);
+                if (existingProduct != null)
+                {
+                    existingProduct.Quantity += product.Quantity;
+                }
+                else
+                {
+                    cartItems.Add(product);
+                }
+            }
+
+            public void RemoveFromCart(string productName)
+            {
+                var product = cartItems.Find(p => p.Name == productName);
+                if (product != null)
+                {
+                    cartItems.Remove(product);
+                }
+            }
+
+            public List<Product> GetCartItems()
+            {
+                return cartItems;
+            }
+
+            public decimal CalculateTotal()
+            {
+                decimal total = 0;
+                foreach (var item in cartItems)
+                {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+
+            public void ClearCart()
+            {
+                cartItems.Clear();
+            }
+        }
+
+        private void btnRemoveFromCart_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
